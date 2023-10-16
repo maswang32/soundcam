7bedf948ac943564a0ba5b220c957857afcaa383
Mason Wang <mwanger32@gmail.com>
2023-10-15 21:55:00 -0700

    Update pk_inference.py

--- posekernel/pk_inference.py
+++ posekernel/pk_inference.py
@@ -67,7 +67,6 @@ if __name__ == "__main__":
         test_indices = np.arange(c.shape[0])
         
     c = c[test_indices, :]
-    pk_save_
     pk_save_path = os.path.join(args.save_dir, "ds_pose_kernels.npy")
     
     print("Loading Pose Kernels")
